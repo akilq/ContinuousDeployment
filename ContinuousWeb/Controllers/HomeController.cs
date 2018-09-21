5980d9d267c55cc53aab6f8eca17aed7fd08f149
akilq <akilesq@gmail.com>
2018-09-21 23:18:19 +0200

    reverted

--- ContinuousWeb/Controllers/HomeController.cs
+++ ContinuousWeb/Controllers/HomeController.cs
@@ -24,7 +24,7 @@ namespace ContinuousWeb.Controllers
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "Your contact pages.";
 
             return View();
         }
